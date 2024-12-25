a05c7ae6c7077a8b2e204e6facbd2ce7353128f6
suyash-vats <suyashvats@google.com>
2024-12-25 08:42:42 +0530

    first

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://enabling-alien-47.clerk.accounts.dev/",
+      domain: "https://relieved-gannet-80.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
